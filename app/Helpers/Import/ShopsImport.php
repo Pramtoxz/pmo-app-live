ea1374c4b6b46091d6a36c5b0d5f38e1aaaaa109
Pramtoxz <pramuditometra@gmail.com>
2026-08-29 11:41:47 +0700

    fix: update default password hash

--- app/Helpers/Import/ShopsImport.php
+++ app/Helpers/Import/ShopsImport.php
@@ -6,7 +6,7 @@ use Illuminate\Support\Facades\DB;
 
 class ShopsImport
 {
-    private const DEFAULT_PASSWORD_HASH = '$2y$10$7g7i7KLU4DFMVJLQ24ucfe/tjZ/gVRn6WYi7CGrlbQp6VZO2d.QFW';
+    private const DEFAULT_PASSWORD_HASH = '$2y$12$xxtttfLNJil.0EMQvH0fhus4BmT2UcOzOKBt16DCAFj4DYa4bC2X2';
 
     public static function process(string $filepath): array
     {
